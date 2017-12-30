58d292709b4ea186f9350cc0b3b8e58542dfd19d
YogaSakti <Yogazacty@gmail.com>
2017-12-30 21:19:24 +0700

    up

--- src/bot.js
+++ src/bot.js
@@ -9,12 +9,12 @@ const LINE = require('./main.js');
 const auth = {
 	authToken: 'EoUtjrObWgRgdLWDnsJc.FPRJAoTY9OwIvv8wHqyyZa.tyloxv3gqtRSexUf//ObBwOrEUi+Uf9NZW2zhewuN/Q=',
 	certificate: 'fcd1eb003833027c7e33cab30f45319c14783c785ce4ed4635b6f3d800fb4430',
-	//email: 'si.andi.kiks@gmail.com',
-	//password: 'ASDer123'
+	email: 'si.andi.kiks@gmail.com',
+	password: 'ASDer123'
 }
 
-//let client =  new LineConnect();
-let client =  new LineConnect(auth);
+let client =  new LineConnect();
+//let client =  new LineConnect(auth);
 
 client.startx().then(async (res) => {
 	while(true) {
